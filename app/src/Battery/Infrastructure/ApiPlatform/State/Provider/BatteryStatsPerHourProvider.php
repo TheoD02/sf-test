14fabb54844e7e9fbe265e5a2151493d2ecf148f
Theo D <theo.d02290@gmail.com>
2024-09-11 23:49:41 +0200

    battery stats

--- app/src/Battery/Infrastructure/ApiPlatform/State/Provider/BatteryStatsPerHourProvider.php
+++ app/src/Battery/Infrastructure/ApiPlatform/State/Provider/BatteryStatsPerHourProvider.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\Operation;
 use App\Battery\Domain\Repository\BatteryRepository;
 use App\Battery\Infrastructure\ApiPlatform\Output\BatteryStatsPerHourOutput;
 use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
 use Rekalogika\ApiLite\State\AbstractProvider;
 use Rekalogika\Mapper\IterableMapperInterface;
 use Symfony\Component\Clock\Clock;
@@ -15,18 +16,17 @@ use Symfony\Component\Clock\Clock;
  */
 class BatteryStatsPerHourProvider extends AbstractProvider
 {
-    use LoggerAwareTrait;
-
     public function __construct(
         private readonly BatteryRepository       $batteryRepository,
         private readonly IterableMapperInterface $mapper,
+        private readonly LoggerInterface         $logger,
     )
     {
     }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        $this->logger?->critical('BatteryStatsPerHourProvider::provide');
+        $this->logger?->critical('Coucou le log !', ['that freaking thing' => 123]);
         return $this->mapper->mapIterable(
             source: $this->batteryRepository->getBatteryStatsPerHour(from: Clock::get()->now()->sub(new \DateInterval('PT10H'))),
             target: BatteryStatsPerHourOutput::class,
