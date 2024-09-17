471560dd94ccbead76312e51257486179054ba6d
Theo D <theo.d02290@gmail.com>
2024-09-17 19:12:18 +0200

    fix

--- .castor/src/setup.php
+++ .castor/src/setup.php
@@ -1,7 +1,10 @@
 <?php
 
+use Castor\Attribute\AsListener;
 use Castor\Attribute\AsTask;
-use function Castor\finder;
+use Castor\Event\BeforeExecuteTaskEvent;
+use Symfony\Component\Process\ExecutableFinder;
+use function Castor\capture;
 use function Castor\io;
 use function Symfony\Component\String\u;
 use function TheoD\MusicAutoTagger\root_context;
@@ -26,9 +29,48 @@ function setup(): void
         root_context()->workingDirectory . '/app/vite.config.js',
     ];
 
+    io()->section("Setting up project with name {$appName}");
     foreach ($files as $file) {
         $contents = file_get_contents($file);
         $contents = str_replace('<app-name-placeholder>', $appName, $contents);
         file_put_contents($file, $contents);
+    }
+
+    io()->success('Project setup complete');
+    io()->info([
+        'You can now run `castor start` to start the project',
+        '',
+        "You can access the app at https://{$appName}.web.localhost after running `castor start`",
+    ]);
+
+    unlink(__FILE__);
+}
+
+#[AsListener(BeforeExecuteTaskEvent::class, priority: PHP_INT_MAX)]
+function check_tool_deps(BeforeExecuteTaskEvent $event): void
+{
+    io()->write('Checking if docker is installed...');
+    if ((new ExecutableFinder())->find('docker') === null) {
+        io()->writeln('<error> KO </error>');
+        io()->error(
+            [
+                'Docker is required for running this application',
+                'Check documentation: https://docs.docker.com/engine/install',
+            ],
+        );
+        exit(1);
+    } else {
+        io()->writeln('<info> OK </info>');
+    }
+
+    io()->write('Checking if traefik container is running...');
+    $output = capture('docker ps');
+
+    if (str_contains($output, 'traefik') === false) {
+        io()->writeln('<error> KO </error>');
+        io()->error('Traefik container is not running. Please start it before running this command.');
+        exit(1);
+    } else {
+        io()->writeln('<info> OK </info>');
     }
 }
