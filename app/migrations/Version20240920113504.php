f92ad855ab3be3a6bd95bd39f19c44854c592766
Theo D <theo.d02290@gmail.com>
2024-09-20 13:39:19 +0200

    infra deploy

--- app/migrations/Version20240920113504.php
+++ app/migrations/Version20240920113504.php
@@ -20,7 +20,7 @@ final class Version20240920113504 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE battery ADD data JSON NOT NULL');
+        $this->addSql('ALTER TABLE battery ADD data JSON NOT NULL DEFAULT \'[]\'');
     }
 
     public function down(Schema $schema): void
